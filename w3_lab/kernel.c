8f39c16cce40b14b032a2b720c7f2852345c68ba
Tran Dang Khoa <trankhoa758@gmail.com>
2023-07-18 09:20:41 +0700

    Update Week 3 Lab

--- w3_lab/kernel.c
+++ w3_lab/kernel.c
@@ -1,21 +1,16 @@
-// -----------------------------------kernel.c -------------------------------------
-#include "gpio.h"
-void delay(){
-//Have some delay
-unsigned int count = 1000000;
-while (count--) { asm volatile("nop"); } //waiting 1000000 cycles
-}
+#include "uart.h"
+
 void main() {
-//Configure GPIO Pin 3 as an output (set bit 9 in GPFSEL0)
-GPFSEL0 |= 1 << 9;
-while (1){
-//Turn the LED at GPIO Pin 3 ON (set bit 3 in GPSET0)
-GPSET0 |= 1 << 3;
-//Have some delay
-delay();
-//Turn the LED at GPIO Pin 3 OFF (set bit 3 in GPCLR0)
-GPCLR0 |= 1 << 3;
-//Have some delay
-delay();
-}
+    // intitialize UART
+    uart_init();
+    // say hello
+    uart_puts("Hello World!\n");
+    // echo everything back
+    while(1) {
+        //read each char
+        char c = uart_getc();
+        //send back twice
+        uart_sendc(c);
+        uart_sendc(c);
+    }
 }
